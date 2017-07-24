6d18365e03c5daa51d6394247b5b7e61ed05d34c
Julie Quinn <Julie@first-interstate.local>
2017-07-24 14:31:02 -0400

    Submit button function

--- App/data/friends.js
+++ App/data/friends.js
@@ -1,38 +1,32 @@
 // Hold all friend users
 var users = [
 	{
-		name: "Zach",
-		photo: "https://s-media-cache-ak0.pinimg.com/564x/7f/fd/c1/7ffdc1785774db3b6b1f15a71304624a.jpg",
+		name: "Zach",,
 		scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 	},
 	
 	{
 		name: "Emily",
-		photo: "http://wakeupcallcoaching.com/wp-content/uploads/2009/10/Alexis-Neeley1.jpg",
 		scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 	},
 	
 	{
 		name: "Stephen",
-		photo: "https://s-media-cache-ak0.pinimg.com/564x/04/b0/10/04b0104d1eefa281b6295fc55a7ee324.jpg",
 		scores: [3, 2, 5, 4, 5, 1, 2, 5, 4, 1]
 	},
 
 	{
 		name: "Nora",
-		photo: "https://s-media-cache-ak0.pinimg.com/564x/7f/fd/c1/7ffdc1785774db3b6b1f15a71304624a.jpg",
 		scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 	},
 	
 	{
 		name: "Susan",
-		photo: "http://wakeupcallcoaching.com/wp-content/uploads/2009/10/Alexis-Neeley1.jpg",
 		scores: [2, 3, 4, 2, 1, 2, 5, 1, 2, 5]
 	},
 	
 	{
 		name: "Colton",
-		photo: "https://s-media-cache-ak0.pinimg.com/564x/04/b0/10/04b0104d1eefa281b6295fc55a7ee324.jpg",
 		scores: [4, 1, 3, 3, 2, 1, 2, 2, 3, 1]
 	}
 ]
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Require dependencies
+// Require modules
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
