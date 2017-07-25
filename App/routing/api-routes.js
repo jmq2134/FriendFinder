dfaa6b34c7ddff4b85e5fb5d3b7306066db4813b
Julie Quinn <Julie@first-interstate.local>
2017-07-25 11:03:42 -0400

    Fixes to Api-routes

--- App/routing/api-routes.js
+++ App/routing/api-routes.js
@@ -3,33 +3,57 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var users = require('../data/friends.js')
 
-// Export Api Routes
+// Connect to express server
+var app = express();
 
-module.exports = function(app) 
-{
-	app.get("/api/friends", function(req,res) 
-	{
-		res.json(users);
-	})
+// When /api/friends is visited, use "get" to display JSON data and export
+exports.getFriends = app.get('/api/friends', function(req, res) {
+	
+	// Display JSON user data from response
+	res.json(users.friends);
+});
 
-	app.post("/api/friends", function (req, res)
-	{
-		var finalDifference = 100;
-		var yourMatch;
-		users.forEach(function(item) 
-		{
-			var differenceNow = 0;
-			for (i=0; i < item.scores.length; i++) 
-			{
-				differenceNow += Math.abs(item.scores[i] - req.body.scores[i]);
-			}
-			if (differenceNow <= finalDifference) 
-			{
-				finalDifference = differenceNow;
-				yourMatch = item;
-			}
-		})
-		res.json(yourMatch);
-		users.push(req.body);
-	})
-}
+// When /api/friends is visited, use "post" to find the best match and export
+exports.postFriends = app.post('/api/friends', function(req, res) {
+	
+	// Hold the user's info
+	var newUser = req.body;
+
+	// Assume the first friend in the users array in friends.js is the best match
+	var friendMatch = 0;
+
+	// Hold the lowest score.  The best match is determined when the lowest score is closest to user's score
+	var lowScore;
+
+	// Find the score of the first friend and set to lowScore
+	for (var j = 0; j < users.friends[0].scores.length; j++) {
+
+		// Low score is the absolute value of the new user's score - the current friend's score
+		lowScore += Math.abs(newUser.scores[j] - users.friends[0].scores[j]);
+	}
+
+	// Check if any other users have a lower score than the current best match
+	for (var i = 1; i < users.friends.length; i++) {
+
+		// Hold the value of the score of the current friend chosen
+		var newScore = 0;
+
+		// Low score is the absolute value of the new user's score - the current friend's score
+		for (var j = 0; j < users.friends[i].scores.length; j++) {
+			newScore += Math.abs(newUser.scores[j] - users.friends[i].scores[j]);
+		};
+
+		// if the newScore of the differences in scores is lower than the current low score, then assign the newScore to lowScore
+		if (lowScore > newScore) {
+			lowScore = newScore;
+			friendMatch = i;
+		};
+	} 
+
+	// Push the new user (from survey) to the users list in friends.js
+	users.friends.push(newUser);
+
+	// Return the friendMatch to be used in module on survey page
+	res.send(users.friends[friendMatch]);
+});
+
--- App/routing/html-routes.js
+++ App/routing/html-routes.js
@@ -2,15 +2,21 @@
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// Connect to express server
+var app = express();
+
+
 // Export public html routes
 
 module.exports = function(app) 
 {
+	// If the URL ends with /survey, display survey.html
 	app.get("/survey", function(req,res) 
 	{
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	})
 
+	// Else display home.html
 	app.use(function (req, res)
 	{
 		res.sendFile(path.join(__dirname, "../public/home.html"));
